1ab157fa9bc42735bac616c606b4f7dcdf9440ca
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-10-10 16:50:05 -0400

    HashTable

---
+++ HashTables/lesson1.js
@@ -0,0 +1,13 @@
+let user = {
+  age: 54,
+  name: "Alina",
+  magic: true,
+  scream: function () {
+    console.log("ahhhhhh!");
+  },
+};
+
+user.age;
+user.spell = "abra";
+user;
+console.log(user);
